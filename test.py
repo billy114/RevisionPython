5bf8eea64a0e073662ec643e76d485f09d95b28b
Billel <billel.abbes@hotmail.com>
2025-02-04 15:45:39 +0100

    update

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 import requests
 
 users = requests.get("https://jsonplaceholder.typicode.com/users/").json()
-for user in users:
-    print("name : ", user["name"])
+# for user in users:
+#     print("name : ", user["name"])
+
+# for user in users:
+#     print(user["name"], " habite à ", user["address"]["city"] )
+
+id = input("Donnez l'id : ")
+user = requests.get("https://jsonplaceholder.typicode.com/users/"+id).json()
+print(user["name"], " habite à ", user["address"]["city"] )
+
 
